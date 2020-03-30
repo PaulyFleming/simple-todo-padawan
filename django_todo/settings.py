6651964310e6e5c2c1516aecbc8f0cd6a28f8d7b
Paul FLeming <pfleming@heroku.com>
2020-03-30 16:48:23 +0100

    Updated ALLOWED_HOSTS

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!b0vs#*reyezo9iown1tmm7+&u5ysn(*ez*!vdh^51yms3njzd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simlpe-todo-padawan.herokuapp.com/']
 
 
 # Application definition
